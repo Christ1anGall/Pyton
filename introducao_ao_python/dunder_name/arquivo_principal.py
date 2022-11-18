c3fc90b89f970ed6b6c362b5e2694ef313d23fd0
Christ1anGall <christiangall92@icloud.com>
2022-11-18 17:34:57 -0300

    modulo

---
+++ introducao_ao_python/dunder_name/arquivo_principal.py
@@ -0,0 +1,15 @@
+
+from pacote import modulo_1
+
+print("Dunder name do arquivo principal ->", __name__)
+print("Tipo do valor dunder name do arquivo principal ->", type(__name__))
+
+
+
+
+def main():
+    print("Dunder name do arquivo principal ->", __name__)
+    print("Tipo do valor dunder name do arquivo principal ->", type(__name__))
+
+if __name__ == "__main__":
+    main()
---
+++ introducao_ao_python/dunder_name/pacote/modulo_1.py
@@ -0,0 +1,2 @@
+print("Dunder name do modulo_1 ->", __name__)
+print("Tipo do valor dunder name do modulo_1 ->", type(__name__))
